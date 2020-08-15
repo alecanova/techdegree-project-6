4e76be365ccdb053b8b192dd156ace4c09da338f
alecanova <alessandrocanova14@gmail.com>
2020-08-15 22:45:19 +0200

    Set error handler

--- app.js
+++ app.js
@@ -11,10 +11,33 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-/* Static middleware for serving static files*/
+/* Static middleware for serving static files */
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+/* 404 handler to catch undefined or non-existent route requests */  
+app.use((req, res, next) => {
+
+    const err = new Error('Not Found');
+    err.status = 404;
+    err.message = "Oops! Looks like the page doesn't exist."
+
+    next(err);
+
+  });
+
+  /* Error handler */
+  app.use((err, req, res, next) => {
+
+    // Set locals
+    res.locals.error = err;
+    res.locals.message = err.message;
+    // Render error page
+    res.status(err.status || 500);
+    res.render('error');
+
+  });
 
 /* Start Express server */
 app.listen(3000, () => {
